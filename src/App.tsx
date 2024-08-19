2e10e490c20ffb20236bd01ec1f189c292ef66f3
Alex Chon <achon@uwaterloo.ca>
2024-08-19 11:31:54 -0400

    Delete test branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-<<<<<<< HEAD
       <h1>Hello world!</h1>
-=======
-      <h1>Hello </h1>
->>>>>>> 17f807f6f375ad8da23c89a4402cf9f3be580e3f
     </>
-  )
+  );
 }
 
-export default App
+export default App;

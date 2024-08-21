429e9322797334ab92b37d83fd1af9333c80f5ee
Alex Chon <achon@uwaterloo.ca>
2024-08-21 02:52:30 -0400

    Finish Simplify Text Page FrontEnd

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -2,26 +2,31 @@ import { ChangeEvent, useState } from 'react';
 
 interface TextArea {
   placeholder: string;
+  title?: string;
   value: string;
   style?: React.CSSProperties;
   className?: string;
+  id?: string;
   handleChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  handleKeyPress?: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
+  handleKeyDown?: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
 }
 
 function TextArea(props: TextArea) {
   const [isFocused, setIsFocused] = useState(false);
   return (
     <div className={`textarea-container ${isFocused ? 'focused' : ''}`}>
-      <label htmlFor='keywords' className='label'>
-        Keywords to keep
+      <label htmlFor={props.id} className='label'>
+        {props.title}
       </label>
       <textarea
-        className='textarea'
+        className={props.className}
         placeholder={props.placeholder}
-        id='keywords'
+        id={props.id}
         onChange={props.handleChange}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
+        onKeyDown={props.handleKeyPress}
         value={props.value}
       ></textarea>
     </div>

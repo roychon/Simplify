d81026eb873a04bd551d92abd3cf4d0d29148441
Roy Chon <roychon040423@gmail.com>
2024-08-26 00:39:09 -0400

    create database model

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ const simplifyController = require('./controllers/simplifyController');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const MONGODB_URL = process.env.MONGODB_CONNECTION
 const PORT = process.env.PORT || '3000';
 
 // MIDDLEWARE
@@ -33,10 +34,11 @@ app.post('/simplify-text', simplifyController.simplifyText);
 app.post('/simplify-file', simplifyController.simplifyFile);
 
 // START SERVER
-app
-  .listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  })
-  .on('error', (e) => {
-    console.log(e);
-  });
+app.listen(PORT, async () => {
+  try {
+      await mongoose.connect(MONGODB_URL)
+      console.log(`Server connected on port ${PORT}`)
+  } catch (error) {
+      console.log(error)
+  }
+})

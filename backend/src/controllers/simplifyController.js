0d34fd3df2501f3d71e1870a637346d22da392c4
Roy Chon <roychon040423@gmail.com>
2024-08-26 00:36:39 -0400

    add simplify file logic

--- backend/src/controllers/simplifyController.js
+++ backend/src/controllers/simplifyController.js
@@ -22,97 +22,61 @@ const simplifyText = async (req, res) => {
   res.send(response.text);
 };
 
-const simplifyFile = async (req, res) => {
-  // TODO: handle duplicate file uploads
-
-  if (req.files && Object.keys(req.files).length !== 0) {
-    // console.log("REQ: ", req)
-    // console.log(req.files)
-    // Path to upload file
-    const uploadedFile = req.files.file;
-
-    // Upload path
-    const uploadPath = __dirname + '/uploads/' + shortid.generate() + '.html';
-
-    // saving file logic using mv() function
-    uploadedFile.mv(uploadPath, function (err) {
-      if (err) {
-        console.log(err);
-        res.status(404).send('File failed to save');
-      }
-
-      // read contents of html file, then parse it
-      fs.readFile(uploadPath, 'utf8', async (err, data) => {
-        const dom = new JSDOM(data);
-        const document = dom.window.document;
-        // init Cohere client
-        const { CohereClient } = require('cohere-ai');
-        const cohere = new CohereClient({
-          token: process.env.COHERE_API_KEY,
-        });
-        const simplifyTextNodes = async (node, counter) => {
-          if (counter.count == 9) return;
-          // console.log(document.childNodes[0])
-          for (let child of node.childNodes) {
-            // console.log(`text content: ${child.nodeValue}`)
-            // if (child.nodeValue == null) console.log("NULL\n")
-            if (
-              child.nodeType == 3 &&
-              child.nodeValue != null &&
-              child.nodeValue &&
-              child.nodeValue.trim()
-            ) {
+const simplifyFile = async (err, data) => {
+  const dom = new JSDOM(data)
+  const document = dom.window.document
+  const simplifyTextNodes = async (node, counter) => {
+      if (counter.count == 2) return // TODO: change to 9 afterwards
+      for (let child of node.childNodes) {
+          // console.log(`text content: ${child.nodeValue}`)
+          // if (child.nodeValue == null) console.log("NULL\n")
+          if (child.nodeType == 3 && child.nodeValue != null && child.nodeValue && child.nodeValue.trim()) {
               // console.log("HEEE")
               const content = child.nodeValue.trim();
               console.log(`CONTENT: ${content}`);
 
               try {
-                const chat = await cohere.chat({
-                  model: 'command',
-                  message: `Simplify the following text to a Grade 6 Reading Level by replacing complex words with simpler synonyms. 
-                                  Only return the simplified text, without any explanations, introductions, or conclusions: "${content}".`,
-                });
+                  const chat = await cohere.chat({
+                      model: "command",
+                      message: `Simplify the following text to a Grade 6 Reading Level by replacing complex words with simpler synonyms. 
+                      Only return the simplified text, without any explanations, introductions, or conclusions: "${content}".`
+                  });
 
-                // Replace the original text with the simplified text
-                const firstQuoteIndex = chat.text.indexOf('"');
-                const secondQuoteIndex = chat.text.lastIndexOf('"');
-                child.nodeValue = chat.text.slice(
-                  firstQuoteIndex + 1,
-                  secondQuoteIndex
-                );
-                // console.log(`SIMPLIFIED TEXT: ${chat.text}`)
+                  // Replace the original text with the simplified text
+                  const firstQuoteIndex = chat.text.indexOf('"');
+                  const secondQuoteIndex = chat.text.lastIndexOf('"');
+                  child.nodeValue = chat.text.slice(firstQuoteIndex + 1, secondQuoteIndex);
+                  // console.log(`SIMPLIFIED TEXT: ${chat.text}`)
               } catch (error) {
-                console.error(`Error simplifying text: ${error.message}`);
+                  console.error(`Error simplifying text: ${error.message}`);
               }
               counter.count += 1;
-              console.log('COUNT: ', counter.count);
-            }
-            await simplifyTextNodes(child, counter);
+              console.log("COUNT: ", counter.count);
           }
-        };
-        // in js, numbers are not by reference
-        let counter = { count: 0 };
-        // recursive function call
-        await simplifyTextNodes(document.body, counter);
-        //     Serialize the modified DOM back to an HTML string
-        const simplifiedHtml = dom.serialize();
+          await simplifyTextNodes(child, counter)
+      }
+  };
+  // in js, numbers are not by reference
+  let counter = {count: 0}
+  // recursive function call
+  await simplifyTextNodes(document.body, counter)
+  //     Serialize the modified DOM back to an HTML string
+  const simplifiedHtml = dom.serialize();
 
-        // Write the simplified HTML back to the original file
-        fs.writeFile(uploadPath, simplifiedHtml, 'utf8', (err) => {
-          if (err) {
-            console.log(err);
-            return res.status(500).send('Failed to write simplified file');
-          }
+  // Write the simplified HTML back to the original file
+  fs.writeFile(uploadPath, simplifiedHtml, 'utf8', (err) => {
+    if (err) {
+        console.log(err);
+        return res.status(500).send('Failed to write simplified file');
+    }
 
-          console.log('DONE');
-          return res
-            .status(201)
-            .send('Successfully simplified contents of file');
-          // TODO: save to MongoDB database and send the file entry locations to frontend later.
-        });
-      });
-    });
-  }
-};
+    // console.log('DONE');
+    const base64Encoding = fs.readFileSync(uploadPath, {encoding: "base64"})
+    console.log(base64Encoding)
+    return res.status(201).json({"fileEncoding": base64Encoding});
+    // TODO: save to MongoDB database and send the file entry locations to frontend later.
+  })
+  
+}
 
 module.exports = { simplifyText, simplifyFile };
--- frontend/src/pages/SimplifyFile.tsx
+++ frontend/src/pages/SimplifyFile.tsx
@@ -8,7 +8,7 @@ const SimplifyFile = () => {
     const fileRef = useRef<HTMLInputElement|null>(null)
     const [file, setFile] = useState<File | null>(null)
     const [isFileChosen, setIsFileChosen] = useState<boolean>(true)
-    const [isFileSimplified, setIsFileSimplified] = useState<boolean>(false)
+    const [fileEncoding, setFileEncoding] = useState<string>("")
     const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             setFile(e.target.files[0])
@@ -20,32 +20,58 @@ const SimplifyFile = () => {
     }, [file])
     
     return (
-        <>
+      <>
+        {fileEncoding ? (
+          <PDFViewer fileEncoding={fileEncoding} />
+        ) : (
           <section id="file-upload-page">
             <HomePageTitle title={"Upload your document"} />
-            <div style={{"textAlign": "left"}}>
-                <p>Drag and drop your file here. We accept .html and .docx files</p>
-                <input ref={fileRef} type="file" id="file-upload-btn" onChange={(e) => handleFileUpload(e)} accept="html" style={{"marginTop": "10px"}}/>
-            {file && (
-              <section style={{"marginTop": "10px"}}>
-                File details:
-                <ul>
-                  <li>Name: {file.name}</li>
-                  <li>Type: {file.type}</li>
-                </ul>
-              </section>
-            )}
-
+            <div style={{ textAlign: "left" }}>
+              <p>Drag and drop your file here. We accept .html and .docx files</p>
+              <input
+                ref={fileRef}
+                type="file"
+                id="file-upload-btn"
+                onChange={(e) => handleFileUpload(e)}
+                accept=".html, .docx"
+                style={{ marginTop: "10px" }}
+              />
+              {file && (
+                <section style={{ marginTop: "10px" }}>
+                  File details:
+                  <ul>
+                    <li>Name: {file.name}</li>
+                    <li>Type: {file.type}</li>
+                  </ul>
+                </section>
+              )}
             </div>
             <div className="file-btn-section">
-              <Button text="Submit" handleClick={() => file ? sendFileToBackend(file) : setIsFileChosen(false)} />
-              <Button text="Cancel" handleClick={() => { if (fileRef) fileRef.current.value = null; setFile(null);}} />
+              <Button
+                text="Submit"
+                handleClick={async () => {
+                  if (file) {
+                    const res = await sendFileToBackend(file);
+                    console.log(res);
+                    setFileEncoding(res);
+                  } else {
+                    setIsFileChosen(false);
+                  }
+                }}
+              />
+              <Button
+                text="Cancel"
+                handleClick={() => {
+                  if (fileRef.current) fileRef.current.value = "";
+                  setFile(null);
+                }}
+              />
             </div>
             {!isFileChosen && <p>No file chosen</p>}
           </section>
-        {isFileSimplified && <PDFViewer />}
-        </>
-      );
+        )}
+      </>
+    );    
       
 }
  

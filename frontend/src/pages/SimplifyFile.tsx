64630ae3d91b739117c0701e9d1a747abeaec364
Roy Chon <roychon040423@gmail.com>
2024-08-26 02:55:12 -0400

    add loading logic to simplify file

--- frontend/src/pages/SimplifyFile.tsx
+++ frontend/src/pages/SimplifyFile.tsx
@@ -9,6 +9,7 @@ const SimplifyFile = () => {
     const [file, setFile] = useState<File | null>(null)
     const [isFileChosen, setIsFileChosen] = useState<boolean>(true)
     const [fileEncoding, setFileEncoding] = useState<string>("")
+    const [isLoading, setIsLoading] = useState<boolean>(false)
     const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             setFile(e.target.files[0])
@@ -21,54 +22,62 @@ const SimplifyFile = () => {
     
     return (
       <>
-        {fileEncoding ? (
-          <PDFViewer fileEncoding={fileEncoding} />
+        {isLoading ? (
+          <div className='loading-page'>
+            <div className='loading-indicator'>Loading...</div>
+          </div>
         ) : (
-          <section id="file-upload-page">
-            <HomePageTitle title={"Upload your document"} />
-            <div style={{ textAlign: "left" }}>
-              <p>Drag and drop your file here. We accept .html and .docx files</p>
-              <input
-                ref={fileRef}
-                type="file"
-                id="file-upload-btn"
-                onChange={(e) => handleFileUpload(e)}
-                accept=".html, .docx"
-                style={{ marginTop: "10px" }}
-              />
-              {file && (
-                <section style={{ marginTop: "10px" }}>
-                  File details:
-                  <ul>
-                    <li>Name: {file.name}</li>
-                    <li>Type: {file.type}</li>
-                  </ul>
-                </section>
-              )}
-            </div>
-            <div className="file-btn-section">
-              <Button
-                text="Submit"
-                handleClick={async () => {
-                  if (file) {
-                    const res = await sendFileToBackend(file);
-                    console.log(res);
-                    setFileEncoding(res);
-                  } else {
-                    setIsFileChosen(false);
-                  }
-                }}
-              />
-              <Button
-                text="Cancel"
-                handleClick={() => {
-                  if (fileRef.current) fileRef.current.value = "";
-                  setFile(null);
-                }}
-              />
-            </div>
-            {!isFileChosen && <p>No file chosen</p>}
-          </section>
+          fileEncoding ? (
+            <PDFViewer fileEncoding={fileEncoding} />
+          ) : (
+            <section id="file-upload-page">
+              <HomePageTitle title={"Upload your document"} />
+              <div style={{ textAlign: "left" }}>
+                <p>Drag and drop your file here. We accept .html and .docx files</p>
+                <input
+                  ref={fileRef}
+                  type="file"
+                  id="file-upload-btn"
+                  onChange={(e) => handleFileUpload(e)}
+                  accept=".html, .docx"
+                  style={{ marginTop: "10px" }}
+                />
+                {file && (
+                  <section style={{ marginTop: "10px" }}>
+                    File details:
+                    <ul>
+                      <li>Name: {file.name}</li>
+                      <li>Type: {file.type}</li>
+                    </ul>
+                  </section>
+                )}
+              </div>
+              <div className="file-btn-section">
+                <Button
+                  text="Submit"
+                  handleClick={async () => {
+                    if (file) {
+                      setIsLoading(true)
+                      const res = await sendFileToBackend(file);
+                      console.log(res);
+                      setFileEncoding(res);
+                      setIsLoading(false)
+                    } else {
+                      setIsFileChosen(false);
+                    }
+                  }}
+                />
+                <Button
+                  text="Cancel"
+                  handleClick={() => {
+                    if (fileRef.current) fileRef.current.value = "";
+                    setFile(null);
+                  }}
+                />
+              </div>
+              {!isFileChosen && <p>No file chosen</p>}
+            </section>
+          )
         )}
       </>
     );    
